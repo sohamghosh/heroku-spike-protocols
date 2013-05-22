b3a3bda1da10d58d61f144d327cf49cca43e25b0
Soham Ghosh <sohamghosh@gmail.com>
2013-05-22 12:12:12 +0530

    Flag to disable SMPP by default. Fix2.

--- src/main/java/org/motechproject/spike/TestController.java
+++ src/main/java/org/motechproject/spike/TestController.java
@@ -20,7 +20,7 @@ public class TestController {
     @ResponseBody
     @RequestMapping("/smpp")
     public String smpp() throws Exception {
-        if (smppService.isEnabled()) {
+        if (!smppService.isEnabled()) {
             return "SMPP is not enabled. Refer to readme.";
         }
         smppService.sendMessage();
